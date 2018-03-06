b827fb082bad812572d0b17745f06f38f0336f63
Jeff <fengisbback@163.com>
2018-03-06 23:15:24 +0800

    第一个程序

--- mainProject.py
+++ mainProject.py
@@ -1,1 +1,18 @@
 from flask import Flask
+from flask import request
+from flask.ext.script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello,%s!</h1>' % name
+
+if __name__ == '__main__':
+    manager.run()
