50d6402e05709ffe150ed51cb9fde03c50d8f9f9
Jeff <fengisbback@163.com>
2018-03-06 23:41:08 +0800

    no message

--- mainProject.py
+++ mainProject.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask import request
 from flask.ext.script import Manager
 
@@ -7,12 +7,11 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is %s</p>' % user_agent
+    return render_template('index.htlm')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello,%s!</h1>' % name
+    return render_template('user.htlm',name=name)
 
 if __name__ == '__main__':
     manager.run()
