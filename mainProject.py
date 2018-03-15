525ed23795b99ab798eeb8a6ccb95dbaa40eaad7
Jeff <fengisbback@163.com>
2018-03-15 21:39:05 +0800

    no message

--- mainProject.py
+++ mainProject.py
@@ -4,17 +4,25 @@ from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringFied,SubmitFied
+from wtform.validators import Required
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class NameForm(Form):
+    name = StringFied('What is your name?',valitators=[Required()])
+    submit = SubmitFied('Submit')
+
 @app.route('/')
 def index():
     return render_template('index.html',
-                           current_time=datetime.utnow())
+                           current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
