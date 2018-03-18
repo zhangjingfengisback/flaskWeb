aecce9c3b2a3e1e8ca901d80d735d3bd0ae850d4
Jeff <fengisbback@163.com>
2018-03-18 22:41:08 +0800

    no message

--- mainProject.py
+++ mainProject.py
@@ -1,28 +1,44 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask import request
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
 from flask.ext.wtf import Form
-from wtforms import StringFied,SubmitFied
-from wtform.validators import Required
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-class NameForm(Form):
-    name = StringFied('What is your name?',valitators=[Required()])
-    submit = SubmitFied('Submit')
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer,primary_key==True)
+    name = db.Column(db.String(64),unique=True)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?')
+    submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
