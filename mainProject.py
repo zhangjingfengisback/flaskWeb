d1d8c87d91953bc3c76757c5ae799d6882176ddb
Jeff <fengisbback@163.com>
2018-03-19 23:17:56 +0800

    no message

--- mainProject.py
+++ mainProject.py
@@ -9,20 +9,42 @@ from wtforms import StringField,SubmitField
 from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+from flask.ext.script import Shell
+#from flask.ext.migrate import Migrate,MigrateCommand
+from flask.ext.mail import Message
+from flask.ext.mail import Mail
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['FLASY_MSIL_SENDER'] = 'Flasky Admin <jeff@example.com>'
+app.config['FLASK_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+mail = Mail(app)
+
+def send_async_email(app,msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to,subject,template,**kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,
+                  sender=app.config['FLASY_MAIL_SENDER'],recipients=[to])
+    msg.body = render_template(template + '.txt',**kwargs)
+    msg.html = render_template(template + '.html',**kwargs)
+    thr = Thread(target = send_async_email,args=[app,msg])
+    thr.start()
+    return thr
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
-    id = db.Column(db.Integer,primary_key==True)
+    id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64),unique=True)
     users = db.relationship('User',backref = 'role')
 
@@ -42,16 +64,31 @@ class NameForm(Form):
     name = StringField('What is your name?')
     submit = SubmitField('Submit')
 
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command("shell",Shell(make_context=make_shell_context))
+
+
 @app.route('/',methods=['GET','POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.fileter_by(username=form.name.data).first()
+        if user is None:
+            user = user(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'],'New User',
+                           mail/new_user,user=user)
+        else:
+            session['kown'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html',form=form,name=session.get('name'))
+    return render_template('index.html',
+            form = form,name = session.get('name'),
+            kown = session.get('known',False))
 
 @app.route('/user/<name>')
 def user(name):
