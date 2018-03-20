5af18190bbcb7316b9793b9e19e4f6a6f33af13a
Jeff <fengisbback@163.com>
2018-03-20 12:35:57 +0800

    no message

---
+++ __init__.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import config
+from flask import Blueprint
+from . import views,errors
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+main = Blueprint('main',__name__)
+
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    return app
